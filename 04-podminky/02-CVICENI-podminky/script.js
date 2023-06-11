fdab022d52c3d6363f01c021a0fbdb0aacbb1a45
Luděk Roleček <ludek@rolecek.cz>
2023-06-11 12:17:00 +0200

    Přidány Podmínky

---
+++ 04-podminky/02-CVICENI-podminky/script.js
@@ -0,0 +1,71 @@
+console.log('%c CVIČENÍ: Podmínky ', 'background:black;color:yellow;');
+
+/*
+Cvičení 1: Pivo
+Máte životně důležitý úkol. Nesmíte zklamat! Použijte podmínku `if` a dejte mi výpisem do konzole vědět, jestli je moje pivo podmírák (tj. objem je méně než 0.5 litru). Pokud je vše v pořádku, nemusíte vypisovat nic (ale můžete).
+*/
+const objem = 0.46;
+
+
+
+/*
+Cvičení 2: Heslo
+Nechte uživatele pomocí `prompt()` zadat heslo. Pokud zadal správné a extrémně bezpečné heslo `heslo123`, napište do konzole, že ho vítáte v tajné části aplikace. Pokud zadal heslo špatně, odkažte ho do místa, kam ☀ nesvítí (tj. do sklepa).
+*/
+const spravneHeslo = 'heslo123';
+
+
+
+/*
+Cvičení 3: Klady a zápory
+Nechte uživatele zadat pomocí `prompt()` číslo. Do konzole vypište, zda jde o kladné nebo záporné číslo, nebo zda je číslo nula.
+*/
+
+
+
+/*
+Cvičení 4: Sleva
+V programu mějte proměnnou s celkovou hodnotou zboží v košíku. Je-li v košíku zboží za více jak 1000 Kč, dostane zákazník slevu 10 %. Pokud je hodnota zboží větší jak 3000 Kč, dostane slevu 20 %. Do konzole vypište, kolik zákazník za svoje zboží zaplatí.
+*/
+
+
+
+/*
+Cvičení 5: Muž nebo žena?
+Máme textový řetězec s rodným číslem ve formátu RRMMDD/XXXX. Druhé dvojčíslí představuje měsíc narození a u žen je k tomuto číslu přičteno `50`. Pokud se tedy žena narodila v lednu, bude mít jako měsíc v rodném čísle uvedeno 51, pokud by se narodila v prosinci, bude tam mít 62.
+
+Napište program, který z textového řetězce získá druhé dvojčíslí a podle něho zjistí, zda jde o může nebo ženu a výsledek vypíše do konzole.
+
+Pro účely příkladu vynecháme jakékoliv kontroly na potenciální pravost/nepravost rodného čísla a budeme řešit pouze druhé dvojčíslí s měsícem.
+*/
+
+
+
+/*
+Cvičení 6: Známky
+Nechte uživatele zadat, kolik bodů získal v testu (číslo mezi 0 a 100). Do konzole vypište, jakou písmennou známku dostal. A pro 91-100 bodů, B pro 81-90 bodů, C pro 71-80 bodů, D pro 61-70 bodů, E pro 51-60 bodů. 50 a méně bodů znamená propadnutí se známkou F.
+*/
+
+
+
+/*
+Cvičení 7: Pobyt na měsíci 🌖
+Představte si, že žijete na Měsíci. Teploty se tam běžně pohybují od -173°C do +127°C. Než vylezete z domu, nejdříve zjistěte a do konzole vypište, zda je venku teplota vhodná na kraťasy a tričko s krátkým rukávem, tj. teplota mezi 20°C a 38°C.
+*/
+
+
+
+/*
+Cvičení 8: Pobyt na měsící pro mezinárodní klienty
+Vycházejte ze stejného předpokladu, jako ve cvičení 3. Tentokrát ale máte na měsíci hotel a přijde za vámi host a ptá se “Venku je 64 stupňů. Mám si vzít kraťasy nebo raději skafandr?” Bohužel jsou mezi hosty i američani, a vy nevíte, zda vám host teplotu říká jako normální člověk ve stupních Celsia, nebo po americku ve stupních Fahrenheita.
+
+Napište program, který se pomocí `prompt()` zeptá:
+- kolik je venku stupňů
+- zda host říká teplotu ve stupních Celsia nebo Fahrenheita (zadá `C` nebo `F`)
+
+Pokud host odpoví, že teplotu říká ve stupních Fahrenheita, musíte ji nejprve převést na stupně Celsia. Vzorec pro převod je `celsius = (fahrenheit - 32) * 0.5556`
+
+Pohybuje-li se teplota mezi 20°C a 38°C, doporučíte kraťasy a tričko 🩳, jinak skafandr 👩‍🚀.
+*/
+
+
---
+++ 04-podminky/04-CVICENI-logicke-operatory/script.js
@@ -0,0 +1,76 @@
+console.log('%c CVIČENÍ: Logické operátory ', 'background:black;color:yellow;');
+
+/*
+Cvičení 1: Přístup do knihovny
+Mějte dvě proměnné. V jedné bude uloženo, zda má uživatel členskou kartičku do knihovny. Ve druhé bude uloženo, zda je knihovna právě otevřená nebo ne. Obě proměnné mohou být typu boolean.
+
+Má-li uživatel kartičku a knihovna je zrovna otevřená, napište do konzole “Přístup povolen”. Jinak napište “Přístup nepovolen”.
+
+Bonus: Pod “Přístup nepovolen” napište i důvod, proč uživatel do knihovny nemůže. Není členem nebo knihovna není otevřená?
+*/
+
+
+
+/*
+Cvičení 2: Ovoce
+Moje oblíbené ovoce je jahoda, meloun nebo hruška. Nechte uživatele pomocí `prompt()` zadat, jaké ovoce mi nabízí (např. “borůvky”). Do konzole vypište, zda si dnes zamlsám nebo ne (tj. zjistěte,  zda zadané ovoce je jahoda, meloun nebo hruška).
+*/
+
+
+
+/*
+Cvičení 3: Velikost trička
+Nechte uživatele zadat velikost trička (zadá jednu z velikostí SX, S, M, L, XL). Do konzole vypište, zda je tričko skladem nebo ho musíme objednat. Bohužel nemáme skladem velikosti S a M.
+*/
+
+
+
+/*
+Cvičení 4: Přestupný rok
+Napište program, který zjistí, zda se jedná o přestupný rok. Přestupný rok je dělitelný číslem `4`. Roky dělitelné číslem `100` jsou ale přestupné jen tehdy, pokud jsou zároveň dělitelné číslem `400`.
+*/
+
+
+
+/*
+Cvičení 5: Víkend
+Nechte uživatele zadat den v týdnu (zadá např. “úterý”). Napište proram, který do konzole vypíše, zda se jedná o pracovní den nebo víkend.
+*/
+
+
+
+/*
+Cvičení 6: Roční období
+Nechte uživatele zadat číslo měsíce 1-12. Do konzole vypište, o jaké roční období se jedná (jaro, léto, podzim, zima).
+*/
+
+
+
+/*
+Cvičení 7: Login
+Nechte uživatele zadat přihlašovací jméno a heslo. Jde-li o uživatele `mysicka47` s heslem `90210`, napíšeme do konzole “Přihlášení proběhlo úspěšně”, jinak napíšeme “Špatné jméno nebo heslo”.
+*/
+
+
+
+/*
+Cvičení 8: Hipster kavárna
+Provozujete malou kavárnu. Nechte uživatele zadat 2 hodnoty:
+- jakou chce kávu (např. espresso, capucino, flat white, latte)
+- jak velkou ji chce (mala, stredni, velka)
+
+Jsme kávoví snobové, takže podáváme pouze malé espresso nebo střední capucino. Pokud si uživatel vyžádal jednu z těchto dvou variant, vypište do konzole “Objednávka přijata”, jinak napište “Tuto kombinaci nepodáváme”.
+*/
+
+
+
+/*
+Cvičení 9: Výběr z bankomatu
+V programu máte dvě proměnné. V jedné je, kolik peněz má uživatel na účtu. V druhé je, kolik peněz má bankomat k dispozici. Nechte uživatele zadat, kolik peněz si chce vybrat.
+
+Má-li dostatek peněz na účtu a bankomat má celou částku k dispozici, napište do konzole “Výběr v pořádku prooveden”.
+
+V opačném případě napište “Chyba při výběru”. Při chybě napište, zda je důvodem nedostatek peněz na účtu nebo v bankomatu. Je-li málo peněz v bankomatu, napište i kolik maximálně může bankomat vydat.
+*/
+
+
